38b2e96f79fe303f826aec7a2326cbdc6fb2c4d3
squizm@hotmail.com <squizm@hotmail.com>
2021-06-24 15:07:55 -0300

    Added dependencies and got player moving

--- main.py
+++ main.py
@@ -1,16 +1,48 @@
-# This is a sample Python script.
+import tcod
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from actions import EscapeAction, MovementAction
+from input_handlers import EventHandler
 
+def main() -> None:
+    screen_width = 80
+    screen_height = 50
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    player_x = int(screen_width/2)
+    player_y = int(screen_height/2)
 
+    tileset = tcod.tileset.load_tilesheet(
+        "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
+    )
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    event_handler = EventHandler()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    with tcod.context.new_terminal(
+            screen_width,
+            screen_height,
+            tileset=tileset,
+            title="Squizm's RL2021",
+            vsync=True,
+    ) as context:
+        root_console = tcod.Console(screen_width, screen_height, order="F")
+        while True:
+            root_console.print(x=player_x, y=player_y, string="@")
+
+            context.present(root_console)
+            root_console.clear()
+
+            for event in tcod.event.wait():
+
+                action = event_handler.dispatch(event)
+
+                if action is None:
+                    continue
+
+                if isinstance(action, MovementAction):
+                    player_x += action.dx
+                    player_y += action.dy
+
+                elif isinstance(action, EscapeAction):
+                    raise SystemExit()
+
+if __name__ == "__main__":
+    main()
